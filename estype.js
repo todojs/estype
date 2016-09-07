294cfe2bdf69d53f7a359f294f61c4124c59726b
Pablo Almunia <Pablo Almunia>
2016-09-07 22:01:02 +0200

    regex optimization

--- estype.js
+++ estype.js
@@ -1,23 +1,34 @@
-function type(value) {
+(function (root) {
     "use strict";
-    var r;
-    if (typeof value === 'object') {
-        if (value === null) {
-            return 'null';
-        }
-        if (typeof value.constructor === 'function' &&
-            (r = value.constructor.name) !== 'Object') {
-            if (r === '' || r === undefined) {
-                return Function.prototype.toString.call(value.constructor)
-                    .match(/^\n?(?:function|class)\s*([^\s(]*)/)[1] || 'anonymous';
+    function type(value) {
+        var r;
+        if (typeof value === 'object') {
+            if (value === null) {
+                return 'null';
             }
-            return r;
+            if (typeof value.constructor === 'function' &&
+                (r = value.constructor.name) !== 'Object') {
+                if (r === '' || r === undefined) {
+                    return Function.prototype.toString.call(value.constructor)
+                        .match(/^\n?(?:function|class)\s*([^\s(]*)/)[1] || 'anonymous';
+                }
+                return r;
+            }
+            return Object.prototype.toString.call(value).match(/\s(.*)\]/)[1];
+        } else if (typeof value === 'number') {
+            return isNaN(value) ? 'NaN' : 'number';
         }
-        return Object.prototype.toString.call(value).match(/\s(.*)\]/)[1];
-    } else if (typeof value === 'number') {
-        return isNaN(value) ? 'NaN' : 'number';
+        return typeof value;
     }
-    return typeof value;
-}
 
-module.exports = type;
+    // Export for node and browser
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            module.exports = type;
+        }
+        exports.type = type;
+    } else {
+        root.type = type;
+    }
+
+})(this);
